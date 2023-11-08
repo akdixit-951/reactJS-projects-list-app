69a2ef5ff320bcd02f054e9974291e0b58aecfbc
akdixit-951 <142324992+akdixit-951@users.noreply.github.com>
2023-11-08 11:41:27 +0530

    index.js

--- src/components/ProjectItem/index.js
+++ src/components/ProjectItem/index.js
@@ -2,21 +2,27 @@ import './index.css'
 
 const ProjectItem = props => {
   const {projectDetails} = props
-  const {projectId, imageURL, description, title} = projectDetails
+  const {projectId, imageURL, description, title, webUrl} = projectDetails
   return (
-    <>
-      <li className="project-item-container">
-        <img
-          className="project-item-image"
-          src={imageURL}
-          alt={`project-item ${projectId}`}
-        />
-        <div className="project-item-details-container">
-          <h1 className="project-item-title">{title}</h1>
-          <p className="project-item-description">{description}</p>
-        </div>
-      </li>
-    </>
+    <li className="project-item-container">
+      <img
+        className="project-item-image"
+        src={imageURL}
+        alt={`project-item ${projectId}`}
+      />
+      <div className="project-item-details-container">
+        <h1 className="project-item-title">{title}</h1>
+        <p className="project-item-description">{description}</p>
+        <a
+          className="project-link"
+          target="_blank"
+          rel="noreferrer"
+          href={webUrl}
+        >
+          Visit Website
+        </a>
+      </div>
+    </li>
   )
 }
 
